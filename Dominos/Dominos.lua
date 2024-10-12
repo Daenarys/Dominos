fae04813a900fe8256b55d8af9a87beeea3c923e
Daenarys <androidwoz@gmail.com>
2024-10-12 14:26:49 +0200

    use stock bag/menu bars

--- Dominos/Dominos.lua
+++ Dominos/Dominos.lua
@@ -218,8 +218,6 @@ function Addon:GetDatabaseDefaults()
 
             ab = { count = 10, showgrid = true, rightClickUnit = 'player' },
 
-            frames = { bags = { point = 'BOTTOMRIGHT', oneBag = true, keyRing = true, spacing = 2 } },
-
             alignmentGrid = { enabled = true, size = 32 },
 
             -- what modules are enabled
@@ -778,10 +776,6 @@ function Addon:HideBlizzard()
         "PetActionBar",
         "StatusTrackingBarManager",
         "MainMenuBarVehicleLeaveButton",
-        "MicroButtonAndBagsBar",
-        "BagsBar",
-        "MicroMenu",
-        "MicroMenuContainer",
         "PlayerCastingBarFrame"
     )
 
@@ -796,9 +790,6 @@ function Addon:HideBlizzard()
         "StanceBar",
         "PossessActionBar",
         "MainMenuBarVehicleLeaveButton",
-        "BagsBar",
-        "MicroMenu",
-        "MicroMenuContainer",
         "PlayerCastingBarFrame"
     )
 
