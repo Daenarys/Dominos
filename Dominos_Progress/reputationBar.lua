ef99d87e52b0a9ed953177777bad01cf9abb818c
Daenarys <androidwoz@gmail.com>
2026-02-26 16:51:09 +0100

    cleanup

--- Dominos_Progress/reputationBar.lua
+++ Dominos_Progress/reputationBar.lua
@@ -1,94 +1,134 @@
 local _, Addon = ...
 local Dominos = _G.Dominos
 local ReputationBar = Dominos:CreateClass("Frame", Addon.ProgressBar)
+local PARAGON_FACTION_COLOR_INDEX = #FACTION_BAR_COLORS
+
+local GetFriendshipReputation = GetFriendshipReputation
+if not GetFriendshipReputation then
+    GetFriendshipReputation = function()
+        return
+    end
+end
+
+local IsFactionParagon = C_Reputation and C_Reputation.IsFactionParagon
+if not IsFactionParagon then
+    IsFactionParagon = function()
+        return false
+    end
+end
+
+local IsMajorFaction = C_Reputation and C_Reputation.IsMajorFaction
+if not IsMajorFaction then
+    IsMajorFaction = function()
+        return false
+    end
+end
+
+local GetWatchedFactionInfo = GetWatchedFactionInfo
+if not GetWatchedFactionInfo then
+    GetWatchedFactionInfo = function()
+        local data = C_Reputation.GetWatchedFactionData()
+        if not data or data.factionID == 0 then
+            return
+        end
+
+        return data.name,
+            data.reaction,
+            data.currentReactionThreshold,
+            data.nextReactionThreshold,
+            data.currentStanding,
+            data.factionID
+    end
+end
+
+local function IsFriendshipFaction(factionID)
+    if factionID then
+        local getRep = C_GossipInfo and C_GossipInfo.GetFriendshipReputation
+        if type(getRep) == "function" then
+            local info = getRep(factionID)
+
+            if type(info) == "table" then
+                return info.friendshipFactionID > 0
+            end
+        end
+    end
+
+    return false
+end
 
 function ReputationBar:Init()
     self:Update()
 end
 
 function ReputationBar:Update()
-    local watchedFactionData = C_Reputation.GetWatchedFactionData()
-    if not watchedFactionData or watchedFactionData.factionID == 0 then
-        return
-    end
+    local name, reaction, min, max, value, factionID = GetWatchedFactionInfo()
+    local capped = false
+    local factionStandingText, color
 
-    local factionID = watchedFactionData.factionID
-    local isShowingNewFaction = self.factionID ~= factionID
-    if isShowingNewFaction then
-        self.factionID = factionID
-        local reputationInfo = C_GossipInfo.GetFriendshipReputation(factionID)
-        self.friendshipID = reputationInfo.friendshipFactionID
-    end
+    if not name then
+        min = 0
+        max = 1
+        value = 0
 
-    -- do something different for friendships
-    local level
-    local maxLevel = MAX_REPUTATION_REACTION
+        factionStandingText = REPUTATION
+        color = FACTION_BAR_COLORS[1]
+    elseif IsFactionParagon(factionID) then
+        local currentValue, threshold = C_Reputation.GetFactionParagonInfo(factionID)
 
-    local minBar, maxBar, value = watchedFactionData.currentReactionThreshold, watchedFactionData.nextReactionThreshold, watchedFactionData.currentStanding
-    if C_Reputation.IsFactionParagonForCurrentPlayer(factionID) then
-        local currentValue, threshold, _, hasRewardPending = C_Reputation.GetFactionParagonInfo(factionID)
-        minBar, maxBar  = 0, threshold
-        if currentValue and threshold then
-            value = currentValue % threshold
-        end
-        level = maxLevel
-        if hasRewardPending then
-            value = value + threshold
-        end
-    elseif C_Reputation.IsMajorFaction(factionID) then
-        local majorFactionData = C_MajorFactions.GetMajorFactionData(factionID)
-        minBar, maxBar = 0, majorFactionData.renownLevelThreshold
-        level = majorFactionData.renownLevel
-    elseif self.friendshipID > 0 then
-        local repInfo = C_GossipInfo.GetFriendshipReputation(factionID)
-        local repRankInfo = C_GossipInfo.GetFriendshipReputationRanks(factionID)
-        level = repRankInfo.currentLevel
-        if repInfo.nextThreshold then
-            minBar, maxBar, value = repInfo.reactionThreshold, repInfo.nextThreshold, repInfo.standing
+        min = 0
+        max = threshold
+        value = currentValue % threshold
+
+        color = FACTION_BAR_COLORS[PARAGON_FACTION_COLOR_INDEX]
+        factionStandingText = GetText("FACTION_STANDING_LABEL" .. reaction, UnitSex("player"))
+    elseif IsFriendshipFaction(factionID) then
+        local info = C_GossipInfo.GetFriendshipReputation(factionID)
+
+        if info.nextThreshold then
+            min = info.reactionThreshold
+            max =  info.nextThreshold
+            value = info.standing
         else
-            -- max rank, make it look like a full bar
-            minBar, maxBar, value = 0, 1, 1
+            min = 0
+            max = 1
+            value = 1
+            capped = true
         end
+
+        color = FACTION_BAR_COLORS[reaction]
+        factionStandingText = info.reaction
+    elseif IsMajorFaction(factionID) then
+        local info = C_MajorFactions.GetMajorFactionData(factionID)
+
+        capped  = C_MajorFactions.HasMaximumRenown(factionID)
+        min = 0
+        max = info.renownLevelThreshold
+        value = capped and info.renownLevelThreshold or info.renownReputationEarned or 0;
+
+        color = BLUE_FONT_COLOR
+        factionStandingText = RENOWN_LEVEL_LABEL:format(info.renownLevel)
     else
-        level = watchedFactionData.reaction
+        if reaction == MAX_REPUTATION_REACTION then
+            min = 0
+            max = 1
+            value = 1
+            capped = true
+        end
+
+        color = FACTION_BAR_COLORS[reaction]
+        factionStandingText = GetText("FACTION_STANDING_LABEL" .. reaction, UnitSex("player"))
     end
 
-    local isCapped = (level and maxLevel) and level >= maxLevel
+    max = max - min
+    value = value - min
 
-    -- Normalize values
-    maxBar = maxBar - minBar
-    value = value - minBar
-    if isCapped and maxBar == 0 then
-        maxBar = 1
-        value = 1
-    end
-    minBar = 0
-
-    self:SetValues(value, maxBar)
-
-    local name = watchedFactionData.name
-    local needsAccountWideLabel = C_Reputation.IsAccountWideReputation(factionID)
-    if needsAccountWideLabel then
-        name = name .. " " .. REPUTATION_STATUS_BAR_LABEL_ACCOUNT_WIDE
-    end
-
-    if isCapped then
-        self:SetText(name)
-    else
-        name = name.." %d / %d"
-        self:SetText(name:format(value, maxBar))
-    end
-
-    local color = FACTION_BAR_COLORS[watchedFactionData.reaction]
-    if C_Reputation.IsMajorFaction(factionID) then
-        self:SetColor(BLUE_FONT_COLOR.r, BLUE_FONT_COLOR.g, BLUE_FONT_COLOR.b)
-    else
-        self:SetColor(color.r, color.g, color.b)
-    end
+    self:SetColor(color.r, color.g, color.b)
+    self:SetValues(value, max)
+    self:UpdateText(name, value, max, factionStandingText, capped)
 end
 
 function ReputationBar:IsModeActive()
-    return C_Reputation.GetWatchedFactionData() ~= nil
+    return GetWatchedFactionInfo() ~= nil
 end
 
 -- register this as a possible progress bar mode
