1be5a14d1cb3edafcff8490a184a3480b53ae3cd
Daenarys <androidwoz@gmail.com>
2026-03-01 23:34:11 +0100

    sync 10.x skin 12.0.1

--- Dominos/menuBar.lua
+++ Dominos/menuBar.lua
@@ -62,6 +62,32 @@ function MenuBar:Skin(button)
 
     button:SetSize(28, 36)
 
+    local buttons = {
+        {button = ProfessionMicroButton, name = "SpellbookAbilities"}
+    }
+
+    local function replaceAtlases(self, name)
+        local prefix = "UI-HUD-MicroMenu-"
+        self:SetNormalAtlas(prefix..name.."-Up")
+        self:SetPushedAtlas(prefix..name.."-Down")
+        self:SetDisabledAtlas(prefix..name.."-Disabled")
+
+        self:HookScript("OnUpdate", function()
+            if(self:GetButtonState() == "NORMAL") then 
+                self:SetHighlightAtlas(prefix..name.."-Mouseover", "BLEND")
+            else 
+                self:SetHighlightAtlas(prefix..name.."-Down", "ADD")
+            end 
+        end)
+    end
+
+    local function replaceAllAtlases()
+        for _, data in pairs(buttons) do
+            replaceAtlases(data.button, data.name)
+        end
+    end
+    replaceAllAtlases()
+
     hooksecurefunc("HelpOpenWebTicketButton_OnUpdate", function(self)
         self:SetParent(MainMenuMicroButton)
         self:ClearAllPoints()
