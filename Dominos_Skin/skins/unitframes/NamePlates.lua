2c92ba4c6572a9b06caa15add3f24b9b8e374e6a
Daenarys <androidwoz@gmail.com>
2026-03-11 00:10:29 +0100

    sync 10.x skin 12.0.1

---
+++ Dominos_Skin/skins/unitframes/NamePlates.lua
@@ -0,0 +1,204 @@
+hooksecurefunc(NamePlateAurasMixin, "RefreshList", function(self)
+    if self:IsForbidden() then return end
+
+    if self.BuffListFrame then
+        self.BuffListFrame:SetAlpha(0)
+    end
+
+    for auraItemFrame in self.auraItemFramePool:EnumerateActive() do
+        if not auraItemFrame.Border then
+            auraItemFrame.Border = auraItemFrame:CreateTexture(nil, "BACKGROUND")
+            auraItemFrame.Border:SetAllPoints(auraItemFrame)
+            auraItemFrame.Border:SetColorTexture(0, 0, 0, 1)
+        end
+        if auraItemFrame.Cooldown then
+            auraItemFrame.Cooldown:SetHideCountdownNumbers(true)
+        end
+    end
+end)
+
+local function SkinCastbar(self)
+    if self:IsForbidden() then return end
+
+    if self.Text then
+        self.Text:ClearAllPoints()
+        self.Text:SetPoint("TOPLEFT", 0, -1)
+        self.Text:SetPoint("BOTTOMRIGHT")
+    end
+
+    hooksecurefunc(self, 'HandleInterruptOrSpellFailed', function(_, event)
+        if ( self.Text ) then
+            if ( event == "UNIT_SPELLCAST_FAILED" ) then
+                self.Text:SetText(FAILED)
+            else
+                self.Text:SetText(INTERRUPTED)
+            end
+        end
+    end)
+
+    hooksecurefunc(self, 'SetIsHighlightedCastTarget', function()
+        if self.CastTargetIndicator then
+            self.CastTargetIndicator:Hide()
+        end
+    end)
+
+    hooksecurefunc(self, 'SetIsHighlightedImportantCast', function()
+        if self.ImportantCastIndicator then
+            self.ImportantCastIndicator:Hide()
+        end
+
+        if self.ImportantCastFlashAnim then
+            self.ImportantCastFlashAnim:SetPlaying(false)
+        end
+    end)
+end
+
+local function SkinHealthBar(frame)
+    local isTarget = frame.healthBar:IsTarget()
+
+    frame.healthBar.barTexture:SetTexture("Interface\\TargetingFrame\\UI-TargetingFrame-BarFill")
+    frame.healthBar.bgTexture:SetAlpha(0)
+    frame.healthBar.selectedBorder:SetAlpha(0)
+    frame.healthBar.deselectedOverlay:SetAlpha(0)
+
+    if not frame.background then
+        frame.background = frame:CreateTexture(nil, "BACKGROUND")
+        frame.background:SetAllPoints(frame)
+        frame.background:SetColorTexture(0.2, 0.2, 0.2, 0.85)
+    end
+
+    if not frame.border then
+        frame.border = CreateFrame("Frame", nil, frame, "CpBorderTemplate")
+
+        PixelUtil.SetWidth(frame.border.Left, 1, 2)
+        PixelUtil.SetPoint(frame.border.Left, "TOPRIGHT", frame.border, "TOPLEFT", 0, 1, 0, 2)
+        PixelUtil.SetPoint(frame.border.Left, "BOTTOMRIGHT", frame.border, "BOTTOMLEFT", 0, -1, 0, 2)
+
+        PixelUtil.SetWidth(frame.border.Right, 1, 2)
+        PixelUtil.SetPoint(frame.border.Right, "TOPLEFT", frame.border, "TOPRIGHT", 0, 1, 0, 2)
+        PixelUtil.SetPoint(frame.border.Right, "BOTTOMLEFT", frame.border, "BOTTOMRIGHT", 0, -1, 0, 2)
+
+        PixelUtil.SetHeight(frame.border.Bottom, 1, 2)
+        PixelUtil.SetPoint(frame.border.Bottom, "TOPLEFT", frame.border, "BOTTOMLEFT", 0, 0)
+        PixelUtil.SetPoint(frame.border.Bottom, "TOPRIGHT", frame.border, "BOTTOMRIGHT", 0, 0)
+
+        PixelUtil.SetHeight(frame.border.Top, 1, 2)
+        PixelUtil.SetPoint(frame.border.Top, "BOTTOMLEFT", frame.border, "TOPLEFT", 0, 0)
+        PixelUtil.SetPoint(frame.border.Top, "BOTTOMRIGHT", frame.border, "TOPRIGHT", 0, 0)
+    end
+
+    for i, texture in ipairs(frame.border.Textures) do
+        if isTarget then
+            texture:SetColorTexture(1, 1, 1, 0.9)
+        else
+            texture:SetColorTexture(0, 0, 0, 1)
+        end
+    end
+
+    hooksecurefunc(frame.healthBar, "UpdateSelectionBorder", function()
+        local isTarget = frame.healthBar:IsTarget()
+
+        for i, texture in ipairs(frame.border.Textures) do
+            if isTarget then
+                texture:SetColorTexture(1, 1, 1, 0.9)
+            else
+                texture:SetColorTexture(0, 0, 0, 1)
+            end
+        end
+    end)
+end
+
+local function ShowBorder(frame)
+    if frame.background then
+        frame.background:Show()
+    end
+    if frame.border then
+        frame.border:Show()
+    end
+end
+
+local function HideBorder(frame)
+    if frame.background then
+        frame.background:Hide()
+    end
+    if frame.border then
+        frame.border:Hide()
+    end
+end
+
+local function GetSafeNameplate(unit)
+    local nameplate = C_NamePlate.GetNamePlateForUnit(unit)
+    if not nameplate or not nameplate.UnitFrame then return nil, nil end
+
+    local frame = nameplate.UnitFrame
+    return nameplate, frame
+end
+
+local function HandleNamePlateAdded(unit)
+    local nameplate, frame = GetSafeNameplate(unit)
+    if not frame then return end
+
+    if not frame.castBar.skinned then
+        SkinCastbar(frame.castBar)
+        frame.castBar.skinned = true
+    end
+
+    if not frame.HealthBarsContainer.skinned then
+        SkinHealthBar(frame.HealthBarsContainer)
+        frame.HealthBarsContainer.skinned = true
+    end
+
+    if UnitNameplateShowsWidgetsOnly(unit) then
+        HideBorder(frame.HealthBarsContainer)
+    else
+        ShowBorder(frame.HealthBarsContainer)
+    end
+
+    if frame.selectionHighlight then
+        frame.selectionHighlight:SetTexture("Interface\\TargetingFrame\\UI-TargetingFrame-BarFill")
+        frame.selectionHighlight:SetAlpha(0.25)
+        frame.selectionHighlight:SetBlendMode("ADD")
+        frame.selectionHighlight:SetAllPoints(frame.HealthBarsContainer)
+    end
+
+    hooksecurefunc(frame, "UpdateAnchors", function()
+        frame.castBar:SetHeight(20)
+        frame.castBar:ClearAllPoints()
+        frame.castBar:SetPoint("BOTTOMLEFT", frame, "BOTTOMLEFT")
+        frame.castBar:SetPoint("BOTTOMRIGHT", frame, "BOTTOMRIGHT")
+        frame.castBar.BorderShield:SetSize(16, 18)
+        frame.castBar.Icon:SetSize(18, 18)
+        frame.castBar.Icon:ClearAllPoints()
+        frame.castBar.Icon:SetPoint("CENTER", frame.castBar, "LEFT")
+        frame.castBar.Text:SetTextHeight(14)
+        frame.ClassificationFrame:ClearAllPoints()
+        frame.ClassificationFrame:SetPoint("RIGHT", frame.HealthBarsContainer, "LEFT", -4, 0)
+        if (frame.ClassificationFrame.classificationIndicator) then
+            frame.ClassificationFrame.classificationIndicator:SetScale(1.4)
+        end
+        PixelUtil.SetHeight(frame.HealthBarsContainer, 12)
+        frame.name:SetFontObject("CpSystemFont_LargeNamePlate")
+        frame.name:SetIgnoreParentScale(true)
+        frame.name:SetJustifyH("CENTER")
+        frame.name:ClearAllPoints()
+        PixelUtil.SetPoint(frame.name, "BOTTOM", frame.HealthBarsContainer, "TOP", 0, 4)
+        if frame.HealthBarsContainer.healthBar:IsTarget() or frame.name:IsShown() then
+            frame.AurasFrame.DebuffListFrame:SetPoint("BOTTOM", frame.name, "TOP", 0, 10)
+        else
+            frame.AurasFrame.DebuffListFrame:SetPoint("BOTTOM", frame.name, "TOP", 0, -18)
+        end
+    end)
+end
+
+local f = CreateFrame("Frame")
+f:RegisterEvent("PLAYER_LOGIN")
+f:RegisterEvent("NAME_PLATE_UNIT_ADDED")
+f:SetScript("OnEvent", function(self, event, unit)
+    if event == "PLAYER_LOGIN" then
+        if C_CVar.GetCVar("nameplateStyle") ~= "5" then
+            C_CVar.SetCVar("nameplateStyle", Enum.NamePlateStyle.Legacy)
+        end
+    elseif event == "NAME_PLATE_UNIT_ADDED" then
+        HandleNamePlateAdded(unit)
+    end
+end)
