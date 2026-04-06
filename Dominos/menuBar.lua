409e59c5d6766a7ae050a17d2b6b6af876a5f1b4
Daenarys <androidwoz@gmail.com>
2026-04-06 17:59:56 +0200

    adjust pos

--- Dominos/menuBar.lua
+++ Dominos/menuBar.lua
@@ -102,8 +102,8 @@ function MenuBar:GetDefaults()
     return {
         displayLayer = 'LOW',
         point = 'BOTTOMRIGHT',
-        x = -244,
-        y = 0
+        x = 0,
+        y = 48
     }
 end
 
