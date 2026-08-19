8e6b0e53aa2b8c4531fea85e5b342a57a93d9c3a
Daenarys <androidwoz@gmail.com>
2026-08-19 19:37:09 +0200

    this too

--- Dominos_Skin/skins/LossOfControl.lua
+++ Dominos_Skin/skins/LossOfControl.lua
@@ -1,4 +1,3 @@
 if not _G.LossOfControlFrame then return end
 
-LossOfControlFrame.Cooldown:SetEdgeTexture("Interface\\Cooldown\\edge-LoC")
 LossOfControlFrame.Cooldown:SetSwipeColor(0.17, 0, 0, 0.8)
